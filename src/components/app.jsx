636b78dc886e3d4c6d96407288a738c33a212aa8
千田大貴 <hiroki@appurunoMacBook-puro.local>
2018-08-13 23:40:45 +0900

    react環境作成

---
+++ src/components/app.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import Greeting from './greeting';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Bobda',
+    };
+  }
+
+  handleNameChange(name) {
+    this.setState({ name });
+  }
+
+  render() {
+    return (
+      <div>
+      <input
+        type="text"
+        value={this.state.name}
+        onChange={e => this.handleNameChange(e.target.value)}
+      >
+        <Greeting name={this.state.name} />
+      </div>
+    );
+  }
+}
+
+export default App;
